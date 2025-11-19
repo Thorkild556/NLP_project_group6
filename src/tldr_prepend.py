f2183f29cde157b8a2b0cfcd78d1964a83848d2f
Thorkild Kappel <au716040@cas.au.dk>
2025-11-19 16:54:30 +0100

    multiple prepends

--- src/tldr_prepend.py
+++ src/tldr_prepend.py
@@ -8,7 +8,7 @@ class Prepends:
             "This transcript ({a}) summarizes to:",
             "Transcript {a} can be summarized as:",
             "Video {a} covered:",
-            "In transcript {a}, the main points were:",
+            "In video transcript {a}, the main points were:",
             "Transcript {a} discussed:",
             "The key takeaways from transcript {a}:",
             "Video transcript {a} explained:",
@@ -18,7 +18,7 @@ class Prepends:
             "In video {a}, the content was:",
             "Transcript {a} breakdown:",
             "The main ideas in transcript {a}:",
-            "Video {a} summary:",
+            "Video transcript {a} summary:",
             "Transcript {a} outlined:",
             "Key points from transcript {a}:",
             "The {a} video discussed:",
@@ -38,20 +38,23 @@ class Prepends:
             "Transcript {a} unpacked:",
             "What video {a} was about:",
             "Transcript {a} detailed:",
-            "The crux of transcript {a}:",
             "Video {a} addressed:",
-            "Transcript {a} conveyed:",
-            "Central themes in transcript {a}:",
             "Video {a} illuminated:"
         ]
-    
+
     def get_random_prepend(self, num):
         rand =  random.randint(0, len(self.prepends))
         prep = self.prepends[rand].format(a=num)
         return prep
 
+    def get_n_prepends(self, n):
+        preps = []
+        for i in range(n):
+            preps.append(self.get_random_prepend(i+1))
+        return preps
+
 
 if __name__ == "__main__":
     prep = Prepends()
 
-    print(prep.get_random_prepend(2))
+    print(prep.get_n_prepends(3))
