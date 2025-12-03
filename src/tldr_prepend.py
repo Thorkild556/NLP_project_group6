de949f3733bc926b85b226a418b59d8a680c7ca1
Thorkild Kappel <au716040@cas.au.dk>
2025-12-03 17:31:19 +0100

    tldr_preproc

--- src/tldr_prepend.py
+++ src/tldr_prepend.py
@@ -16,22 +16,22 @@ class Prepends:
             "Transcript {a} highlights:",
             "The {a} transcript focused on:",
             "In video {a}, the content was:",
-            "Transcript {a} breakdown:",
+            "Transcript {a} breakdown is:",
             "The main ideas in transcript {a}:",
             "Video transcript {a} summary:",
             "Transcript {a} outlined:",
-            "Key points from transcript {a}:",
+            "Key points from transcript {a} included:",
             "The {a} video discussed:",
             "Transcript {a} in brief:",
             "Transcript {a} touched on:",
-            "The essence of transcript {a}:",
+            "The essence of transcript {a} was:",
             "Video {a} explored:",
             "Transcript {a} went over:",
             "What transcript {a} covered:",
             "The gist of transcript {a}:",
             "Video {a} delved into:",
             "Transcript {a} presented:",
-            "Core content from transcript {a}:",
+            "Core content from transcript {a} was:",
             "Transcript {a} revealed:",
             "Video {a} examined:",
             "The substance of transcript {a}:",
@@ -41,10 +41,24 @@ class Prepends:
             "Video {a} addressed:",
             "Video {a} illuminated:"
         ]
+        self.preppreps = [
+            "However, ",
+            "Additionally, ",
+            "Furthermore, "
+        ]
 
     def get_random_prepend(self, num):
         rand =  random.randint(0, len(self.prepends))
-        prep = self.prepends[rand].format(a=num)
+        if num == 1:
+            prep = self.prepends[rand].format(a=num)
+        else:
+            rand_prepprep = random.randint(0, len(self.preppreps))
+            prepprep = self.preppreps[rand_prepprep]
+            rando = random.choice(2)
+            if rando == 0:
+                prep = prepprep + (self.prepends[rand].format(a=num).lower())
+            else:
+                prep = self.prepends[rand].format(a=num)
         return prep
 
     def get_n_prepends(self, n):
@@ -57,4 +71,6 @@ class Prepends:
 if __name__ == "__main__":
     prep = Prepends()
 
-    print(prep.get_n_prepends(3))
+    preps = prep.get_n_prepends(4)
+    for i in preps:
+        print(i)
