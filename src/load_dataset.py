5b586f57c855b0593e2ea066dd3b33a2eea72087
RahulARanger <saihanumarahul66@gmail.com>
2025-12-19 23:47:43 +0530

    merge post_fine_tune.ipynb with export_dataset.ipynb

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import Union
 import json
 from datasets import Dataset, DatasetDict
+import matplotlib.pyplot as plt
 
 
 def load_jsonl(file_path: Union[str, Path]) -> Dataset:
@@ -43,3 +44,24 @@ def print_sample(train_dataset) -> None:
 
 CS_JSON = "custom_dataset.jsonl"
 TL_DR_JSON = "proc_tldr.jsonl"
+
+
+def plt_tokens_for_dataset(tokenized_custom_dataset: Dataset, limit: int):
+    sie = []
+    note = []
+
+    for index, _ in enumerate(tokenized_custom_dataset):
+        if len(_['input_ids']) == limit:
+            note.append(index)
+        sie.append(len(_['input_ids']))
+
+    # Create a color list: red if in note, blue otherwise
+    colors = ['red' if i in note else 'blue' for i in range(len(sie))]
+
+    plt.figure(figsize=(12, 6))
+    plt.scatter(range(len(sie)), sie, c=colors)
+    plt.xlabel("Index")
+    plt.ylabel("Length of input_ids")
+    plt.title("Scatter plot of input_id lengths")
+    plt.grid(False)
+    plt.show()
